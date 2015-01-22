bc2970f260bc9ad0f05dcbaecb008109ddc94dba
itrascastro <i.trascastro@gmail.com>
2015-01-22 13:24:55 +0100

    Dependency Injection
    Now the model is passed as a dependency to the controller in the
    constructor

--- public/index.php
+++ public/index.php
@@ -18,17 +18,28 @@ chdir(dirname(__DIR__));
 
 require 'application/controllers/IndexController.php';
 require 'application/controllers/CalculatorController.php';
+require 'application/models/CalculatorModel.php';
 
 if (!isset($_GET['controller']))
 {
     $controllerName = 'controllers\\IndexController';
     $action         = 'indexAction';
+
+    $controller = new $controllerName();
+    $controller->$action();
 }
 else
 {
     $controllerName     = 'controllers\\' . ucfirst($_GET['controller']) . 'Controller';
     $action             = $_GET['action'] . 'Action';
+
+    // Dependeny
+    $model = new models\CalculatorModel();
+
+    //Dependency Inyection
+    $controller = new $controllerName($model);
+    $controller->$action();
 }
 
-$controller = new $controllerName();
-$controller->$action();
+
+
